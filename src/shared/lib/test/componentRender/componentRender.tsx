664692e3768f7decd96c36151277589dbf0fdee6
Kirill Fedotov <rtdkirill@gmail.com>
2024-01-29 19:27:41 +0400

    fixed problems

---
+++ src/shared/lib/test/componentRender/componentRender.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { MemoryRouter } from 'react-router-dom'
+import i18n from 'shared/config/i18n/i18nForTest'
+
+export interface componentRenderOptions {
+    route?: string
+}
+
+export function componentRender(Component: ReactNode, options: componentRenderOptions = {}) {
+    const {
+        route = '/',
+    } = options
+
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18n}>
+                {Component}
+            </I18nextProvider>
+        </MemoryRouter>,
+    )
+}
