a3fb8c5ff4a43bb291a616527f7768c04ce25cf0
Kirill Fedotov <rtdkirill@gmail.com>
2024-04-15 18:25:22 +0400

    add report to CI

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -16,7 +16,7 @@ export default {
         ['jest-html-reporters', {
             publicPath: '<rootDir>/reports/unit',
             filename: 'report.html',
-            openReport: true,
+            // openReport: true,
         }],
     ],
     // All imported modules in your tests should be mocked automatically
